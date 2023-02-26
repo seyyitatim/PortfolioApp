7477e74778fcd4b40d5f92e02d72beb2198eaa38
seyyitatim <seyyitatim@gmail.com>
2023-02-26 23:30:06 +0300

    Add seed data

--- PortfolioApp/PortfolioApp.Data/DbContexts/AppDbContext.cs
+++ PortfolioApp/PortfolioApp.Data/DbContexts/AppDbContext.cs
@@ -13,6 +13,12 @@ namespace PortfolioApp.Data.DbContexts
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Portfolio>()
+                .HasData(new Portfolio {Id=1, Code = "TRY", Name = "Türk Lirası", Amount = 100000 });
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<Portfolio> Portfolios { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
 
---
+++ PortfolioApp/PortfolioApp.Data/Migrations/20230226202738_AddSeedData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioApp.Data.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "rate",
+                table: "Transactions",
+                newName: "Rate");
+
+            migrationBuilder.InsertData(
+                table: "Portfolios",
+                columns: new[] { "Id", "Amount", "Code", "Name" },
+                values: new object[] { 1, 100000m, "TRY", "Türk Lirası" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Portfolios",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.RenameColumn(
+                name: "Rate",
+                table: "Transactions",
+                newName: "rate");
+        }
+    }
+}
